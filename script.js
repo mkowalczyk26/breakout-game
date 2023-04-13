5593d0dd8c49d738f1b268c1a832acb2b8f3818f
Michal <elmowiec1@gmail.com>
2023-04-13 21:33:02 +0200

    new game btn working

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const gameArea = document.querySelector('.main');
 const score = document.querySelector('.score');
 const blockWidth = 100;
 const blockHeight = 20;
@@ -6,215 +5,237 @@ const blockHeight = 20;
 let currentScore = 0
 score.innerHTML = currentScore;
 
-const userStartPosition = [230, 20];
-let currentPosition = userStartPosition;
 
-const ballStartPosition = [270, 60];
-let currentBallPosition = ballStartPosition;
 
 let timer;
 let ballX = 2;
 let ballY = 2;
 
+function newGame() {
+    const gameArea = document.querySelector('.main');
 
-class Block {
-    constructor(x, y) {
-        this.topLeft = [x, y + blockHeight];
-        this.topRight = [x + blockWidth, y + blockHeight];
-        this.bottomLeft = [x, y];
-        this.bottomRight = [x + blockWidth, y];
-    }
-}
+    const userStartPosition = [230, 20];
+    let currentPosition = userStartPosition;
 
-const blocks = [
-    new Block(10, 350),
-    new Block(120, 350),
-    new Block(230, 350),
-    new Block(340, 350),
-    new Block(450, 350),
-    new Block(10, 320),
-    new Block(120, 320),
-    new Block(230, 320),
-    new Block(340, 320),
-    new Block(450, 320),
-    new Block(10, 290),
-    new Block(120, 290),
-    new Block(230, 290),
-    new Block(340, 290),
-    new Block(450, 290),
-]
-//console.log(blocks[0]);
+    const ballStartPosition = [270, 60];
+    let currentBallPosition = ballStartPosition;
 
-function createBlocks() {
-    for (let i = 0; i < blocks.length; i += 1) {
-        const block = document.createElement('div');
-        block.className = 'block';
-        block.style.left = blocks[i].bottomLeft[0] + 'px';
-        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
-        gameArea.appendChild(block);
-    }
-}
-
-createBlocks();
-
-const platform = document.createElement('div');
-platform.className = 'block';
-platform.style.backgroundColor = 'rgb(169, 199, 187)';
-updateUser();
-gameArea.appendChild(platform);
-
-function updateUser() {
-    platform.style.left = currentPosition[0] + 'px';
-    platform.style.bottom = currentPosition[1] + 'px';
-}
-
-
-function movePlatrom(e) {
-    switch(e.key) {
-        case 'ArrowLeft':
-            if (currentPosition[0] > 0)
-            currentPosition[0] -= 10;
-            updateUser();
-            break;
-        case 'ArrowRight':
-            if (currentPosition[0] < 560 - blockWidth)
-            currentPosition[0] += 10;
-            updateUser();
-            break;
-    }
-}
-
-
-document.addEventListener('keydown', movePlatrom);
-
-
-function updateBall() {
-    ball.style.left = currentBallPosition[0] + 'px';
-    ball.style.bottom = currentBallPosition[1] + 'px';
-}
-
-
-const ball = document.createElement('div');
-ball.className = 'ball';
-updateBall();
-gameArea.appendChild(ball);
-
-function moveBall() {
-    checkBallCollisions();
-    currentBallPosition[0] += ballX;
-    currentBallPosition[1] += ballY;
-    updateBall();
-}
-
-timer = setInterval(moveBall, 15);
-
-function checkBallCollisions() {
-    if (
-        currentBallPosition[0] >= 560 - 20 ||
-        currentBallPosition[1] >= 400 - 20 ||
-        currentBallPosition[0] == 0
-        ) {
-            wallCollision();
-
-    } else {
-        userCollision()
-    }
-
-    if (currentBallPosition[1] <= 0) {      //ball hit bottom border
-        clearInterval(timer);
-        const modal = document.createElement('div');
-        modal.className = 'youLostModal';
-        modal.innerText = 'You Lost'
-        modal.appendChild(document.createElement('button'));
-        modal.querySelector('button').className = 'newGameBtn';
-        const newGameBtn = modal.querySelector('.newGameBtn');
-        newGameBtn.innerText = 'New Game'
-        
-        document.body.appendChild(modal);
-        document.querySelector('.container').style.filter = 'blur(3px)';
-    }
-
-    for (let i = 0; i < blocks.length; i += 1) {
-        if (
-            currentBallPosition[0] >= blocks[i].bottomLeft[0] - 20 &&
-            currentBallPosition[0] <= blocks[i].bottomRight[0] + 20 &&
-            currentBallPosition[1] >= blocks[i].bottomLeft[1] - 20 && 
-            currentBallPosition[1] <= blocks[i].topLeft[1] + 20
-        ) {
-            const allBlocks = document.querySelectorAll('.block');
-            //console.log(allBlocks)
-            allBlocks[i].classList.remove('block')
-            blocks.splice(i,1)
-            changeBallDirection()
-            currentScore += 1;
-            score.innerHTML = currentScore;
+    class Block {
+        constructor(x, y) {
+            this.topLeft = [x, y + blockHeight];
+            this.topRight = [x + blockWidth, y + blockHeight];
+            this.bottomLeft = [x, y];
+            this.bottomRight = [x + blockWidth, y];
         }
-        
     }
-    if (blocks.length == 0) {
-        clearInterval(timer);
+    
+    const blocks = [
+        new Block(10, 350),
+        new Block(120, 350),
+        new Block(230, 350),
+        new Block(340, 350),
+        new Block(450, 350),
+        new Block(10, 320),
+        new Block(120, 320),
+        new Block(230, 320),
+        new Block(340, 320),
+        new Block(450, 320),
+        new Block(10, 290),
+        new Block(120, 290),
+        new Block(230, 290),
+        new Block(340, 290),
+        new Block(450, 290),
+    ]
+    //console.log(blocks[0]);
+    
+    function createBlocks() {
+        for (let i = 0; i < blocks.length; i += 1) {
+            const block = document.createElement('div');
+            block.className = 'block';
+            block.style.left = blocks[i].bottomLeft[0] + 'px';
+            block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+            gameArea.appendChild(block);
+        }
     }
-
+    
+    createBlocks();
+    
+    const platform = document.createElement('div');
+    platform.className = 'block';
+    platform.style.backgroundColor = 'rgb(169, 199, 187)';
+    updateUser();
+    gameArea.appendChild(platform);
+    
+    function updateUser() {
+        platform.style.left = currentPosition[0] + 'px';
+        platform.style.bottom = currentPosition[1] + 'px';
+    }
+    
+    
+    function movePlatrom(e) {
+        switch(e.key) {
+            case 'ArrowLeft':
+                if (currentPosition[0] > 0)
+                currentPosition[0] -= 10;
+                updateUser();
+                break;
+            case 'ArrowRight':
+                if (currentPosition[0] < 560 - blockWidth)
+                currentPosition[0] += 10;
+                updateUser();
+                break;
+        }
+    }
+    
+    
+    document.addEventListener('keydown', movePlatrom);
+    
+    
+    function updateBall() {
+        ball.style.left = currentBallPosition[0] + 'px';
+        ball.style.bottom = currentBallPosition[1] + 'px';
+    }
+    
+    
+    const ball = document.createElement('div');
+    ball.className = 'ball';
+    updateBall();
+    gameArea.appendChild(ball);
+    
+    function moveBall() {
+        checkBallCollisions();
+        currentBallPosition[0] += ballX;
+        currentBallPosition[1] += ballY;
+        updateBall();
+    }
+    
+    timer = setInterval(moveBall, 15);
+    
+    function checkBallCollisions() {
+        if (
+            currentBallPosition[0] >= 560 - 20 ||
+            currentBallPosition[1] >= 400 - 20 ||
+            currentBallPosition[0] == 0
+            ) {
+                wallCollision();
+    
+        } else {
+            userCollision()
+        }
+    
+        if (currentBallPosition[1] <= 0) {      //ball hit bottom border
+            clearInterval(timer);
+            const modal = document.createElement('div');
+            modal.className = 'youLostModal';
+            modal.innerText = 'You Lost'
+            modal.appendChild(document.createElement('button'));
+            modal.querySelector('button').className = 'newGameBtn';
+            const newGameBtn = modal.querySelector('.newGameBtn');
+            newGameBtn.innerText = 'New Game';
+            document.body.appendChild(modal);
+            document.querySelector('.container').style.filter = 'blur(3px)';
+    
+            newGameBtn.addEventListener('click', () => {
+                console.log(userStartPosition)
+                modal.remove();
+                document.querySelector('.container').style.filter = 'none';
+                gameArea.remove()
+                //resetValues();
+                const main = document.createElement('div')
+                main.className = 'main';
+                document.querySelector('.container').appendChild(main);
+                currentScore = 0;
+                score.innerHTML = currentScore;
+                newGame();
+            })
+    
+        }
+    
+        for (let i = 0; i < blocks.length; i += 1) {
+            if (
+                currentBallPosition[0] >= blocks[i].bottomLeft[0] - 20 &&
+                currentBallPosition[0] <= blocks[i].bottomRight[0] + 20 &&
+                currentBallPosition[1] >= blocks[i].bottomLeft[1] - 20 && 
+                currentBallPosition[1] <= blocks[i].topLeft[1] + 20
+            ) {
+                const allBlocks = document.querySelectorAll('.block');
+                //console.log(allBlocks)
+                allBlocks[i].classList.remove('block')
+                blocks.splice(i,1)
+                changeBallDirection()
+                currentScore += 1;
+                score.innerHTML = currentScore;
+            }
+            
+        }
+        if (blocks.length == 0) {
+            clearInterval(timer);
+        }
+    
+    }
+    
+    function changeBallDirection() {
+        if (ballX == 2 && ballY == 2) {
+            ballY = -2
+            return
+        }
+        if (ballX == -2 && ballY == 2) {
+            ballY = -2
+            return
+        } 
+        if (ballX == -2 && ballY == -2) {
+            ballY = 2;
+            return
+        }
+        if (ballX == 2 && ballY == -2) {
+            ballX = -2
+            return
+        }
+    
+    }
+    
+    function wallCollision() {
+        if (ballX == 2 && ballY == 2 && currentBallPosition[0] >= 560-20) {
+            ballX = -2
+            return
+        }
+        if (ballX == 2 && ballY == 2 && currentBallPosition[1] >= 400-20) {
+            ballY = -2
+            return
+        }
+        if (ballX == -2 && ballY == 2 && currentBallPosition[0] <= 0) {
+            ballX = 2
+            return
+        } 
+        if (ballX == -2 && ballY == 2 && currentBallPosition[1] >= 400-20) {
+            ballY = -2
+            return
+        } 
+        if (ballX == -2 && ballY == -2 && currentBallPosition[0] <= 0) {
+            ballX = 2;
+            return
+        }
+        if (ballX == -2 && ballY == -2 && currentBallPosition[1] <= 0) {
+            ballY = 2;
+            return
+        }
+        if (ballX == 2 && ballY == -2) {
+            ballX = -2
+            return
+        }
+    }
+    
+    function userCollision() {
+        if(ballX == 2 && ballY == -2 && (currentBallPosition[0] >= currentPosition[0] && currentBallPosition[0] <= currentPosition[0] + 100 && currentBallPosition[1] <= currentPosition[1] + 20)){
+            ballY = 2
+            return
+        }
+        if(ballX == -2 && ballY == -2 && (currentBallPosition[0] >= currentPosition[0] && currentBallPosition[0] <= currentPosition[0] + 100 && currentBallPosition[1] <= currentPosition[1] + 20)){
+            ballY = 2
+            return
+        }
+    }
 }
 
-function changeBallDirection() {
-    if (ballX == 2 && ballY == 2) {
-        ballY = -2
-        return
-    }
-    if (ballX == -2 && ballY == 2) {
-        ballY = -2
-        return
-    } 
-    if (ballX == -2 && ballY == -2) {
-        ballY = 2;
-        return
-    }
-    if (ballX == 2 && ballY == -2) {
-        ballX = -2
-        return
-    }
+newGame()
 
-}
-
-function wallCollision() {
-    if (ballX == 2 && ballY == 2 && currentBallPosition[0] >= 560-20) {
-        ballX = -2
-        return
-    }
-    if (ballX == 2 && ballY == 2 && currentBallPosition[1] >= 400-20) {
-        ballY = -2
-        return
-    }
-    if (ballX == -2 && ballY == 2 && currentBallPosition[0] <= 0) {
-        ballX = 2
-        return
-    } 
-    if (ballX == -2 && ballY == 2 && currentBallPosition[1] >= 400-20) {
-        ballY = -2
-        return
-    } 
-    if (ballX == -2 && ballY == -2 && currentBallPosition[0] <= 0) {
-        ballX = 2;
-        return
-    }
-    if (ballX == -2 && ballY == -2 && currentBallPosition[1] <= 0) {
-        ballY = 2;
-        return
-    }
-    if (ballX == 2 && ballY == -2) {
-        ballX = -2
-        return
-    }
-}
-
-function userCollision() {
-    if(ballX == 2 && ballY == -2 && (currentBallPosition[0] >= currentPosition[0] && currentBallPosition[0] <= currentPosition[0] + 100 && currentBallPosition[1] <= currentPosition[1] + 20)){
-        ballY = 2
-        return
-    }
-    if(ballX == -2 && ballY == -2 && (currentBallPosition[0] >= currentPosition[0] && currentBallPosition[0] <= currentPosition[0] + 100 && currentBallPosition[1] <= currentPosition[1] + 20)){
-        ballY = 2
-        return
-    }
-}
